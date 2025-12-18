9934947333cc0abcd94b66ea2001486152948f0c
“andrewbramwell <andy@voucherstore.io>
2025-12-18 22:49:51 +0800

    i118

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,32 +4,42 @@ import i18n from "@astrolicious/i18n";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://anavareethaimassage.de", // update me!
+  site: "https://anavareethaimassage.de",
   integrations: [
     icon(),
     i18n({
       defaultLocale: "de",
       locales: ["de", "en"],
       client: {
-        data: true,
-        paths: true,
+        data: false,    // ← Changed: Removes locale data from HTML
+        paths: false,   // ← Changed: Removes route mappings from HTML
       },
-      // used to localize the routes
       pages: {
         "/about": {
           en: "/about",
         },
         "/massages": {
           en: "/massages",
+        },
+        "/contact": {
+          en: "/contact",
+        },
+        "/thai-solution-massage": {
+          en: "/thai-solution-massage",
+        },
+        "/impressum": {
+          en: "/impressum",
+        },
+        "/privacy-policy": {
+          en: "/privacy-policy",
         }
       },
     }),
     sitemap({
       i18n: {
-        defaultLocale: 'de', // All urls that don't contain `en` after "https://www.yourwebsite.com/" will be treated as default locale, i.e. `de`
+        defaultLocale: 'de',
         locales: {
-          // key/value pairs of all languages supported
-          de: 'de-DE', // The `defaultLocale` value must be present in `locales` keys
+          de: 'de-DE',
           en: 'en-US',
         },
       },
